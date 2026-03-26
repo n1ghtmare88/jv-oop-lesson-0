2712f16a1d36e82b8b74522cd393fb9ca152effd
Dmitriy Yevtushenko <dmitriy.zp.city@gmail.com>
2026-03-26 12:53:06 +0200

    fixed checkstyle

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -14,7 +14,9 @@ public class AuthenticationService {
      */
     public boolean login(String email, String password) {
         User user = UserService.findByEmail(email);
-        if (user != null && email.equals(user.getEmail()) && password.equals(user.getPassword())) {
+        if (user != null
+                && email.equals(user.getEmail())
+                && password.equals(user.getPassword())) {
             return true;
         }
         return false;
